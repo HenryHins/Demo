447798e6da84212907aec82bcd42a990178fc24a
Ngan Henry <hin-1996820@hotmail.com>
2016-06-21 12:34:06 +0800

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Ngan Chi Hin on 21/6/2016.
+//  Copyright © 2016 Oursky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
